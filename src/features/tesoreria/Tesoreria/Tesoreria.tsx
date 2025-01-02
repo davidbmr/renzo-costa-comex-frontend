661177017e537abe4ac0643b5cb234d679d9c286
Janeth Peves <djaneth.spr@gmail.com>
2025-01-02 15:19:50 -0500

    update: mod de Tesoreria cambios en los datos del dateTable como la fecha y los datos random

--- src/features/tesoreria/Tesoreria/Tesoreria.tsx
+++ src/features/tesoreria/Tesoreria/Tesoreria.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { FaArrowLeft } from "react-icons/fa"; // Ícono para el botón de volver
 import styles from "./Tesoreria.module.css";
 import { MainContentStructure } from "@/components/MainContentStructure/MainContentStructure";
 import { DataTable } from "@/components/DataTable/DataTable";
@@ -8,45 +7,49 @@ import GenerateExcelButton from "@/components/GenerateExcelButton/GenerateExcelB
 import { useNavigate } from "react-router-dom";
 import { useModal } from "@/hooks/useModal";
 import { PrimeModal } from "@/primeComponents/PrimeModal/PrimeModal";
-import { TextBoxField } from "@/components/TextBoxField/TextBoxField";
 import { InputText } from "primereact/inputtext";
 import { AddModal } from "../AddModal/AddModal";
 
 export const Tesoreria = () => {
 	const navigate = useNavigate();
 	const addModal = useModal();
-	// const [showForm, setShowForm] = useState(false);
 	const [data, setData] = useState([]);
 
 	const mapping = {
+		fecha: "Fecha",
     saldoInicialSoles: "Saldo Inicial Soles",
     saldoInicialDolares: "Saldo Inicial Dólares",
     gastosSoles: "Gastos Soles",
     gastosDolares: "Gastos Dólares",
     saldoFinalSoles: "Saldo Final Soles",
     saldoFinalDolares: "Saldo Final Dólares",
+		estado: "Estado",
 	};
 
 	const generateRandomData = (numItems = 50) => {
 
 		return Array.from({ length: numItems }, (_, index) => {
+			const estados = ["En Proceso", "Finalizado"];
 			const cantidad = Math.floor(Math.random() * 100) + 1;
-      const gasto = Math.floor(Math.random() * 10000) + 1;
-      const gastoD = Math.floor(Math.random() * 10000) + 1;
-
-			const precioUnidad = Math.floor(Math.random() * 90) + 10;
+      const gasto = Math.floor(Math.random() * 1000) + 1;
+      const gastoD = Math.floor(Math.random() * 100) + 1;
+			const precioUnidad = Math.floor(Math.random() * 1000) + 10;
 			const total = cantidad * precioUnidad;
+			const saldoFinalSoles = total - gasto;
+			const saldoFinalDolares = precioUnidad - gastoD;
 
 			return {
 				saldoInicialSoles: `S/.${total.toFixed(2)}`,
 				saldoInicialDolares: `US$${precioUnidad.toFixed(2)}`,
         gastosSoles: `S/.${gasto.toFixed(2)}`,
         gastosDolares: `US$${gastoD.toFixed(2)}`,
-				ingreso: new Date(Date.now() - Math.floor(Math.random() * 1e10)).toLocaleDateString(),
+				fecha: new Date(Date.now() - Math.floor(Math.random() * 1e10)).toLocaleDateString(),
 				cantidad,
 				total: `US$${total.toFixed(2)}`,
 				eta: new Date(Date.now() - Math.floor(Math.random() * 1e10)).toLocaleDateString(),
-
+				estado: estados[Math.floor(Math.random() * estados.length)],
+				saldoFinalSoles:  `S/.${saldoFinalSoles.toFixed(2)}`,
+				saldoFinalDolares:  `US$${saldoFinalDolares.toFixed(2)}`,
 			};
 		});
 	};
@@ -102,12 +105,12 @@ export const Tesoreria = () => {
 };
 
 const columns = [
+	{ nombre: "Fecha", campo: "fecha" },
 	{ nombre: "Saldo Inicial Soles", campo: "saldoInicialSoles" },
 	{ nombre: "Saldo Inicial Dólares", campo: "saldoInicialDolares" },
 	{ nombre: "Gastos Soles", campo: "gastosSoles" },
 	{ nombre: "Gastos Dólares", campo: "gastosDolares" },
 	{ nombre: "Saldo Final Soles", campo: "saldoFinalSoles" },
 	{ nombre: "Saldo Final Dólares", campo: "saldoFinalDolares" },
-	{ nombre: "Fecha", campo: "fecha" },
 	{ nombre: "Estado", campo: "estado" },
 ]
