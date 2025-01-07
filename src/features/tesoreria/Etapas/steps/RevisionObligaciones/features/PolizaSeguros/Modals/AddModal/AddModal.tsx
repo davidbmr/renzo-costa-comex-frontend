b587655e9f390d8aa1f1b8638b842971284a3414
CarlosPrad2 <developer2@agents4future.com>
2025-01-06 17:38:47 -0800

    add:poliza

---
+++ src/features/tesoreria/Etapas/steps/RevisionObligaciones/features/PolizaSeguros/Modals/AddModal/AddModal.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import style from "./AddModal.module.css";
+import { Button } from "primereact/button";
+import { TextBoxField } from "@/components/TextBoxField/TextBoxField";
+import { NumberBoxField } from "@/components/NumberBoxField/NumberBoxField";
+import { SelectField } from "@/components/SelectField/SelectField";
+
+interface PropsAddModal {
+  postFetchData?: any;
+  OnHideModal?:any;
+}
+
+export const AddModal = ({ postFetchData,OnHideModal }: PropsAddModal) => {
+  const [formData, setFormData] = useState<any>({
+    seguros: "",
+    proveedor: "",
+    soles: undefined,
+    dolares: undefined,
+    numCuotas: undefined,
+    cuotas: undefined,
+    cuotasCanceladas: undefined,
+    saldo: undefined,
+    numCuotasCanceladas: undefined,
+  });
+
+  const [optionsProveedor, setOptionsProveedor] = useState<any[]>([]);
+  const [newProveedor, setNewProveedor] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | any>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleNumberChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleAddProveedor = () => {
+    if (newProveedor.trim()) {
+      setOptionsProveedor([
+        ...optionsProveedor,
+        { label: newProveedor, value: newProveedor },
+      ]);
+      setNewProveedor("");
+    }
+  };
+
+  const handleSubmit = () => {
+	OnHideModal()
+    if (postFetchData) {
+      postFetchData(formData);
+    }
+  };
+
+  return (
+    <div className={style.modal__container}>
+      <h2>Crear nuevo ítem</h2>
+
+      <div className={style.form__container}>
+        <TextBoxField
+          textLabel="Seguro"
+          name="seguros"
+          value={formData.seguros}
+          onChange={handleChange}
+        />
+        <SelectField
+          textLabel="Proveedor"
+          name="proveedor"
+          value={formData.proveedor}
+          options={proveedores}
+          onChange={handleChange}
+          optionLabel="label"
+          optionValue="value"
+        />
+
+        <NumberBoxField
+          textLabel="Soles"
+          name="soles"
+          value={formData.soles}
+          onChange={handleNumberChange}
+        />
+        <NumberBoxField
+          textLabel="Dólares"
+          name="dolares"
+          value={formData.dolares}
+          onChange={handleNumberChange}
+        />
+        <NumberBoxField
+          textLabel="Nº Cuotas"
+          name="numCuotas"
+          value={formData.numCuotas}
+          onChange={handleNumberChange}
+        />
+        <NumberBoxField
+          textLabel="Cuotas"
+          name="cuotas"
+          value={formData.cuotas}
+          onChange={handleNumberChange}
+        />
+        <NumberBoxField
+          textLabel="Cuotas canceladas"
+          name="cuotasCanceladas"
+          value={formData.cuotasCanceladas}
+          onChange={handleNumberChange}
+        />
+        <NumberBoxField
+          textLabel="Saldo"
+          name="saldo"
+          value={formData.saldo}
+          onChange={handleNumberChange}
+        />
+        <NumberBoxField
+          textLabel="Nº Cuotas Canceladas"
+          name="numCuotasCanceladas"
+          value={formData.numCuotasCanceladas}
+          onChange={handleNumberChange}
+        />
+      </div>
+
+      <div className={style.button__container}>
+        <Button className="p-button-sm p-button-info" onClick={handleSubmit}>
+          Crear
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const proveedores = [
+  { label: "Proveedor A", value: "proveedor_a" },
+  { label: "Proveedor B", value: "proveedor_b" },
+  { label: "Proveedor C", value: "proveedor_c" },
+  { label: "Proveedor D", value: "proveedor_d" },
+  { label: "Proveedor E", value: "proveedor_e" },
+];
--- src/features/tesoreria/Etapas/steps/RevisionObligaciones/features/PolizaSeguros/PolizaSeguros.tsx
+++ src/features/tesoreria/Etapas/steps/RevisionObligaciones/features/PolizaSeguros/PolizaSeguros.tsx
@@ -1,7 +1,101 @@
-import React from 'react'
+import React from "react";
+import style from "./PolizaSeguros.module.css";
+import { useNavigate } from "react-router-dom";
+import { DataTable } from "@/components/DataTable/DataTable";
+import { PrimeModal } from "@/primeComponents/PrimeModal/PrimeModal";
+import { useModal } from "@/hooks/useModal";
+import { AddModal } from "./Modals/AddModal/AddModal";
+import { CustomButton } from "@/components/CustomButton/CustomButton";
+import GenerateExcelButton from "@/components/GenerateExcelButton/GenerateExcelButton";
+import { Divider } from "primereact/divider";
 
 export const PolizaSeguros = () => {
+  const addModal = useModal();
+  const generateRandomData = (numRows = 10) => {
+    const randomNumber = (min, max) =>
+      (Math.random() * (max - min) + min).toFixed(2);
+
+    const seguros = ["onco person", "mult", "onco-rosa", "glob"];
+    const proveedores = ["Pacífico", "La Positiva", "Mapfre", "Rímac", "Sura"];
+    return Array.from({ length: numRows }, () => ({
+      seguros: seguros[Math.floor(Math.random() * seguros.length)],
+      proveedor: proveedores[Math.floor(Math.random() * proveedores.length)],
+      soles: `S/ ${randomNumber(0, 10000)}`,
+      dolares: `$ ${randomNumber(1000, 50000)}`,
+      numCuotas: Math.floor(Math.random() * 12) + 1,
+      cuotas: `S/ ${randomNumber(100, 1000)}`,
+      cuotasCanceladas: `S/ ${randomNumber(100, 5000)}`,
+      saldo: `$ ${randomNumber(500, 10000)}`,
+      numCuotasCanceladas: Math.floor(Math.random() * 12),
+    }));
+  };
+
+  const data = generateRandomData(10);
+
+  const mapping = {
+    seguros: "seguros",
+    proveedor: "proveedor",
+    soles: "soles",
+    dolares: "dolares",
+    numCuotas: "numCuotas",
+    cuotas: "cuotas",
+    cuotasCanceladas: "cuotasCanceladas",
+    saldo: "saldo",
+    numCuotasCanceladas: "numCuotasCanceladas",
+  };
+
   return (
-    <div>PolizaSeguros</div>
-  )
-}
+    <div className={style.poliza__container}>
+      <h2 style={{ color: "#333", textTransform: "uppercase" }}>
+        programación de pagos de pólizas
+      </h2>
+
+      <div className={style.btnContainer}>
+        <CustomButton
+          text="Crear"
+          backgroundButton="#9B1139"
+          colorP="white"
+          onClick={() => addModal.onVisibleModal()}
+        />
+        <CustomButton text="Importar" />
+        <GenerateExcelButton
+          data={data}
+          mapping={mapping}
+          sheetName="Proveedores"
+        />
+      </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        isHeaderActive={false}
+        onEye={() => console.log("ds")}
+      
+
+      />
+
+      <Divider/>
+
+      <PrimeModal
+        header="Agregar COMEX"
+        modalStatus={addModal.modalStatus}
+        onHideModal={addModal.onHideModal}
+        width={600}
+      >
+        <AddModal postFetchData={true} OnHideModal={addModal.onHideModal} />
+      </PrimeModal>
+    </div>
+  );
+};
+
+// Definición de columnas basadas en la tabla de la imagen
+const columns = [
+  { nombre: "Seguro", campo: "seguros" },
+  { nombre: "Proveedor", campo: "proveedor" },
+  { nombre: "Soles", campo: "soles" },
+  { nombre: "Dólares", campo: "dolares" },
+  { nombre: "Nº Cuotas", campo: "numCuotas" },
+  { nombre: "Cuotas", campo: "cuotas" },
+  { nombre: "Cuotas canceladas", campo: "cuotasCanceladas" },
+  { nombre: "Saldo", campo: "saldo" },
+  { nombre: "Nº Cuotas Canceladas", campo: "numCuotasCanceladas" },
+];
