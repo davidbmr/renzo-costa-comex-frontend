9bcdacd78fb9e3e5f7535d937f44bc87a2ce5ffa
Janeth <djaneth.spr@gmail.com>
2025-01-15 14:38:11 -0500

    add: cree un componente de TreeDataTable con cheackbox, update: mod Tesoreria / Saldos Iniciales / Pagos Iniciales Masivos formatee la vista, detele: elimine importaciones que no se utilizan

--- src/components/modals/AddModalProveedores/AddModalProveedores.tsx
+++ src/components/modals/AddModalProveedores/AddModalProveedores.tsx
@@ -1,68 +1,97 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./AddModalProveedores.module.css";
-import { MainContentStructure } from "@/components/MainContentStructure/MainContentStructure";
 import { DataTable } from "@/components/DataTable/DataTable";
 import { TextBoxField } from "@/components/TextBoxField/TextBoxField";
 import { handleChangeInput } from "@/helpers/handleTextBox";
 import { CustomButton } from "@/components/CustomButton/CustomButton";
 
 export const AddModalProveedores = () => {
-	const [filterOpts, setFilterOpts] = useState<any>({
-		cod: "",
-		nombre: "",
-		ruc: "",
-	});
+  const [filterOpts, setFilterOpts] = useState<any>({
+    cod: "",
+    nombre: "",
+    ruc: "",
+  });
 
-	return (
-		<>
-			<div className={styles.filter__container}>
-				<TextBoxField
-					textLabel="Código:"
-					name="cod"
-					value={filterOpts.cod}
-					onChange={(e) => handleChangeInput(e, setFilterOpts)}
-					direction="row"
-					labelWidth="55px"
-					containerWidth="250px"
-				/>
+  return (
+    <>
+      <div className={styles.filter__container}>
+        <TextBoxField
+          textLabel="Código:"
+          name="cod"
+          value={filterOpts.cod}
+          onChange={(e) => handleChangeInput(e, setFilterOpts)}
+          direction="row"
+          labelWidth="55px"
+          containerWidth="250px"
+        />
 
-				<TextBoxField
-					textLabel="Nombre:"
-					name="nombre"
-					value={filterOpts.nombre}
-					onChange={(e) => handleChangeInput(e, setFilterOpts)}
-					direction="row"
-					labelWidth="55px"
-					containerWidth="250px"
-				/>
-				<TextBoxField
-					textLabel="RUC:"
-					name="ruc"
-					value={filterOpts.ruc}
-					onChange={(e) => handleChangeInput(e, setFilterOpts)}
-					direction="row"
-					labelWidth="55px"
-					containerWidth="250px"
-				/>
-				<CustomButton text="Buscar" backgroundButton="#9B1139" colorP="white" onClick={() => ""} />
-			</div>
-			<br />
-			<DataTable columns={columns || []} data={data} isHeaderActive={false} />
-			<br />
-			<CustomButton text="Enviar" backgroundButton="#9B1139" colorP="white" onClick={() => ""} />
-		</>
-	);
+        <TextBoxField
+          textLabel="Nombre:"
+          name="nombre"
+          value={filterOpts.nombre}
+          onChange={(e) => handleChangeInput(e, setFilterOpts)}
+          direction="row"
+          labelWidth="55px"
+          containerWidth="250px"
+        />
+        <TextBoxField
+          textLabel="RUC:"
+          name="ruc"
+          value={filterOpts.ruc}
+          onChange={(e) => handleChangeInput(e, setFilterOpts)}
+          direction="row"
+          labelWidth="55px"
+          containerWidth="250px"
+        />
+        <CustomButton
+          text="Buscar"
+          backgroundButton="#9B1139"
+          colorP="white"
+          onClick={() => ""}
+        />
+      </div>
+      <br />
+      <DataTable columns={columns || []} data={data} isHeaderActive={false} />
+      <br />
+      <CustomButton
+        text="Enviar"
+        backgroundButton="#9B1139"
+        colorP="white"
+        onClick={() => ""}
+      />
+    </>
+  );
 };
 
 const columns = [
-	{ nombre: "Cód", campo: "cod" },
-	{ nombre: "Nombre", campo: "nombre" },
-	{ nombre: "Saldo de Cuenta", campo: "saldoCuenta" },
-	{ nombre: "RUC", campo: "ruc" },
+  { nombre: "Cód", campo: "cod" },
+  { nombre: "Nombre", campo: "nombre" },
+  { nombre: "Saldo de Cuenta", campo: "saldoCuenta" },
+  { nombre: "RUC", campo: "ruc" },
 ];
 const data = [
-	{ cod: "P0001", nombre: "PROVEEDOR 1", saldoCuenta: "0.00", ruc: "20999999998" },
-	{ cod: "P0002", nombre: "PROVEEDOR 2", saldoCuenta: "0.00", ruc: "20799999997" },
-	{ cod: "P0003", nombre: "PROVEEDOR 3", saldoCuenta: "0.00", ruc: "20699999996" },
-	{ cod: "P0004", nombre: "PROVEEDOR 4", saldoCuenta: "0.00", ruc: "20599999995" },
+  {
+    cod: "P0001",
+    nombre: "PROVEEDOR 1",
+    saldoCuenta: "0.00",
+    ruc: "20999999998",
+  },
+  {
+    cod: "P0002",
+    nombre: "PROVEEDOR 2",
+    saldoCuenta: "0.00",
+    ruc: "20799999997",
+  },
+  {
+    cod: "P0003",
+    nombre: "PROVEEDOR 3",
+    saldoCuenta: "0.00",
+    ruc: "20699999996",
+  },
+  {
+    cod: "P0004",
+    nombre: "PROVEEDOR 4",
+    saldoCuenta: "0.00",
+    ruc: "20599999995",
+  },
 ];
--- src/features/tesoreria/Etapas/steps/PagosInicialesMasivos/PagosInicialesMasivos.tsx
+++ src/features/tesoreria/Etapas/steps/PagosInicialesMasivos/PagosInicialesMasivos.tsx
@@ -7,81 +7,87 @@ import { useModal } from "@/hooks/useModal";
 import { TextBoxField } from "@/components/TextBoxField/TextBoxField";
 import { RadioButtonField } from "@/components/RadioButtonField/RadioButtonField";
 import { PrimeModal } from "@/primeComponents/PrimeModal/PrimeModal";
-import { AddModalProveedores } from "@/components/modals/AddModalProveedores/AddModalProveedores";
 import { AddModal } from "./AddModal/AddModal";
 
 export const PagosInicialesMasivos = () => {
-	const addModal = useModal();
-	const options = [
-		{ label: "Generar Pago", value: "P" },
-		{ label: "Generar Archivo TXT", value: "A" },
-		{ label: "Actualizar Referencias de Pagos", value: "R" },
-		{ label: "Eliminar Pre-selección", value: "E" },
-	];
-	const [selectedOption, setSelectedOption] = useState<string | undefined>("opcion1");
-	const handleOptionChange = (value: string) => {
-		setSelectedOption(value);
-	};
+  const addModal = useModal();
+  const options = [
+    { label: "Generar Pago", value: "P" },
+    { label: "Generar Archivo TXT", value: "A" },
+    { label: "Actualizar Referencias de Pagos", value: "R" },
+    { label: "Eliminar Pre-selección", value: "E" },
+  ];
+  const [selectedOption, setSelectedOption] = useState<string | undefined>(
+    "opcion1"
+  );
+  const handleOptionChange = (value: string) => {
+    setSelectedOption(value);
+  };
 
-	return (
-		<>
-			<div className={styles.date__container}>
-				<RadioButtonField
-					title="Parámetros de Filtrado"
-					options={options}
-					name="tipoPago"
-					selectedValue={selectedOption}
-					onChange={handleOptionChange}
-				/>
-				<SelectField
-					textLabel="Tipo de Pago:"
-					name="tipoPago"
-					options={optionTipoPago}
-					value={""}
-					onChange={() => ""}
-					direction="row"
-					labelWidth="140px"
-				/>
-				<SelectField
-					textLabel="Banco:"
-					name="banco"
-					options={optionBanco}
-					value={""}
-					onChange={() => ""}
-					direction="row"
-					labelWidth="140px"
-				/>
-				<TextBoxField
-					textLabel="Descripción de pago:"
-					value={""}
-					name="numeroComprobante"
-					onChange={() => ""}
-					direction="row"
-					labelWidth="140px"
-				/>
-				<CustomButton text="Siguiente" backgroundButton="#9B1139" colorP="white" onClick={addModal.onVisibleModal}/>
-			</div>
+  return (
+    <>
+      <div className={styles.date__container}>
+        <RadioButtonField
+          title="Parámetros de Filtrado"
+          options={options}
+          name="tipoPago"
+          selectedValue={selectedOption}
+          onChange={handleOptionChange}
+        />
+        <SelectField
+          textLabel="Tipo de Pago:"
+          name="tipoPago"
+          options={optionTipoPago}
+          value={""}
+          onChange={() => ""}
+          direction="row"
+          labelWidth="140px"
+        />
+        <SelectField
+          textLabel="Banco:"
+          name="banco"
+          options={optionBanco}
+          value={""}
+          onChange={() => ""}
+          direction="row"
+          labelWidth="140px"
+        />
+        <TextBoxField
+          textLabel="Descripción de pago:"
+          value={""}
+          name="numeroComprobante"
+          onChange={() => ""}
+          direction="row"
+          labelWidth="140px"
+        />
+        <CustomButton
+          text="Siguiente"
+          backgroundButton="#9B1139"
+          colorP="white"
+          onClick={addModal.onVisibleModal}
+        />
+      </div>
 
-			<ConfirmacionEtapa />
+      <ConfirmacionEtapa />
 
-			{/* Add Modal */}
-			<PrimeModal
-				header="Elegir proveedores"
-				modalStatus={addModal.modalStatus}
-				onHideModal={addModal.onHideModal}
-				width={700}
-			>
-				<AddModal postFetchData={true}/>
-			</PrimeModal>
-		</>
-	);
+      {/* Add Modal */}
+      <PrimeModal
+        header="Elegir proveedores"
+        modalStatus={addModal.modalStatus}
+        onHideModal={addModal.onHideModal}
+        width={700}
+      >
+        <AddModal postFetchData={true} />
+      </PrimeModal>
+    </>
+  );
 };
 
 const optionTipoPago = [{ name: "Proveedores", value: "proveedor1" }];
 
 const optionBanco = [
-	{ name: "BCP", value: "proveedor1" },
-	{ name: "BBVA", value: "proveedor2" },
-	{ name: "SCOTIABANK", value: "proveedor3" },
-	{ name: "INTERBANK", value: "proveedor4" },
+  { name: "BCP", value: "proveedor1" },
+  { name: "BBVA", value: "proveedor2" },
+  { name: "SCOTIABANK", value: "proveedor3" },
+  { name: "INTERBANK", value: "proveedor4" },
 ];
