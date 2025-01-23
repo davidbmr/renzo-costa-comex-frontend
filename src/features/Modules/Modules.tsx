b0ba1681d1cc52ba166628c21d9c58681d3b951f
Janeth <djaneth.spr@gmail.com>
2025-01-23 18:52:21 -0500

    update: cambio del primary color en el css global, add: coloque los iconos de los modulos

--- src/features/Modules/Modules.tsx
+++ src/features/Modules/Modules.tsx
@@ -4,6 +4,11 @@ import style from "./Modules.module.css";
 import { useNavigate } from "react-router-dom";
 
 import logo from "@/assets/renzo-costa-transparente.png";
+import { TbWorldPin } from "react-icons/tb";
+import { RiNumbersFill } from "react-icons/ri";
+import { GrMoney } from "react-icons/gr";
+import { DiAptana } from "react-icons/di";
+
 
 export const Modules = () => {
 	const navigate = useNavigate();
@@ -11,6 +16,7 @@ export const Modules = () => {
 	const features = [
 		{
 			id: 1,
+			icon: <TbWorldPin size={40}/>,
 			title: "COMEX",
 			description: "Módulo de comercio exterior",
 			action: "Ingresar al módulo",
@@ -18,6 +24,7 @@ export const Modules = () => {
 		},
 		{
 			id: 2,
+			icon:<RiNumbersFill size={40}/>,
 			title: "Gestión financiera",
 			description: "Módulo de gestión financiera",
 			action: "Ingresar al módulo",
@@ -25,6 +32,7 @@ export const Modules = () => {
 		},
 		{
 			id: 3,
+			icon:<GrMoney size={40}/>,
 			title: "Tesorería",
 			description: "Módulo de tesorería",
 			action: "Ingresar al módulo",
@@ -32,6 +40,7 @@ export const Modules = () => {
 		},
 		{
 			id: 3,
+			icon:<DiAptana size={40}/>,
 			title: "Configuración",
 			description: "Módulo de configuración",
 			action: "Ingresar al módulo",
@@ -47,6 +56,7 @@ export const Modules = () => {
 			<div className={style.gridContainer}>
 				{features.map((feature) => (
 					<div key={feature.id} className={style.featureCard}>
+						<div className={style.icon}>{feature.icon}</div>
 						<h3 className={style.title}>{feature.title}</h3>
 						<p className={style.description}>{feature.description}</p>
 						<button className={style.actionButton} onClick={() => navigate(feature.url)}>
